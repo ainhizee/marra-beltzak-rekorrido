cf1fafa11d2033aa63524fb339cfb935bff588c6
ainhizee <93262250+ainhizee@users.noreply.github.com>
2022-06-01 14:23:25 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
 DFRobotMaqueenPlus.I2CInit()
+let atala_1 = 1
+let atala2 = 0
 basic.forever(function () {
-    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
-    } else {
-        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0) {
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 140)
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 30)
+    if (atala_1 == 1) {
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
         } else {
-            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1) {
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 140)
+                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+            }
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
                 DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 140)
-                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 30)
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
             }
         }
+    }
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L2) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R2) == 1) {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        atala_1 = 0
+        atala2 = 1
+    }
+})
+basic.forever(function () {
+    if (atala2 == 1) {
+    	
     }
 })
