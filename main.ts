201b860928dbea3376af8bb8d269e08542cc072f
ainhizee <93262250+ainhizee@users.noreply.github.com>
2022-06-03 14:26:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,13 +17,17 @@ basic.forever(function () {
         }
     }
     if (DFRobotMaqueenPlus.readPatrol(Patrol.L2) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R2) == 1) {
-        DFRobotMaqueenPlus.mototStop(Motors.ALL)
         atala_1 = 0
         atala2 = 1
     }
 })
 basic.forever(function () {
     if (atala2 == 1) {
-    	
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 60)
+        basic.pause(845)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        atala2 = 0
+        atala_1 = 1
     }
 })
