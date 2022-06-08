f4af08232029b674d7d933b98df7cb309579f4cf
ainhizee <93262250+ainhizee@users.noreply.github.com>
2022-06-08 14:26:11 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ DFRobotMaqueenPlus.I2CInit()
 let atala_1 = 1
 let atala2 = 0
 basic.forever(function () {
+    if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) < 10) {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    }
+})
+basic.forever(function () {
     if (atala_1 == 1) {
         if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
